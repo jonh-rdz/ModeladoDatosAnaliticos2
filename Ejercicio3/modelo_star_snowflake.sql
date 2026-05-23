30a4e2a32b7a5a47228731a8ea7534b5c1156e13
AmayaNicole <amayanicole644@gmail.com>
2026-05-22 18:51:18 -0600

    Subiendo ejercicio 3 y su explicacion

--- Ejercicio3/modelo_star_snowflake.sql
+++ Ejercicio3/modelo_star_snowflake.sql
@@ -1,8 +1,7 @@
-
 CREATE DATABASE IF NOT EXISTS Ejercicio3_DB;
 USE Ejercicio3_DB;
 
-
+-- FACT TABLE
 CREATE TABLE FACT_VENTAS (
     id_venta INT PRIMARY KEY,
     id_producto INT,
@@ -10,14 +9,14 @@ CREATE TABLE FACT_VENTAS (
     precio DECIMAL(10,2)
 );
 
-
+-- ESTRUCTURA STAR
 CREATE TABLE DIM_PRODUCTOS_STAR (
     id_producto INT PRIMARY KEY,
     nombre_producto VARCHAR(100),
     nombre_categoria VARCHAR(50)
 );
 
- 
+-- ESTRUCTURA SNOWFLAKE
 CREATE TABLE DIM_CATEGORIAS (
     id_categoria INT PRIMARY KEY,
     nombre_categoria VARCHAR(50)
@@ -26,6 +25,5 @@ CREATE TABLE DIM_CATEGORIAS (
 CREATE TABLE DIM_PRODUCTOS_SNOW (
     id_producto INT PRIMARY KEY,
     nombre_producto VARCHAR(100),
-    id_categoria INT,
-    FOREIGN KEY (id_categoria) REFERENCES DIM_CATEGORIAS(id_categoria)
+    id_categoria INT
 );
